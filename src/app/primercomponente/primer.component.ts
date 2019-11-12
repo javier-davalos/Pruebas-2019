cbb85639a6bba9b126fa1a9d14ffa5a068d6c63a
javier_davalos <javier_davalos@hotmail.com>
2019-11-12 14:19:21 -0400

    PRUEBAS comit

---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+         Selector:  'helloWorld',
+         template:  '<h1>hello-world</h1>',
+         styles:   ['h1 {background: #000; color; #fff;}']
+})
+export class helloWorld {
+
+}
